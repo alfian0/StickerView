8150005bede3609a81b5606f2a4d004e1c9478bb
alfian0 <alfian.official.mail@gmail.com>
2020-12-16 16:37:52 +0700

    Initial Commit

---
+++ StickerKeyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StickerKeyboard
+//
+//  Created by Macintosh on 16/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
